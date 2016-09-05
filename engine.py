b76f24cea8cdc8a6240754f508c6647bcf8cdab4
sairam kolla <sai.kolla24@gmail.com>
2016-09-05 09:02:31 +0530

    sum,avg,min,max

--- engine.py
+++ engine.py
@@ -2,7 +2,7 @@ from parser import MyParse
 import os
 schema = []
 tables = []
-operations = ["sum","average","max","min"]
+operations = ["sum","average","min","max"]
 def init_engine():
     ot = open("content/welcome.txt","r")
     print ot.read(),
@@ -32,6 +32,62 @@ def find_column(tno,column_name):
             return table.index(col)
     return -1
 
+def sum_col(colno,tno):
+    global tables
+    sum = 0
+    x = open(tables[tno] + '.csv','r')
+    x = x.read()
+    x = x.split('\n')
+    for i in x:
+        sum += int(i.strip().split(',')[colno])
+    return sum
+
+def average_col(colno,tno):
+    global tables
+    sum = 0
+    x = open(tables[tno] + '.csv', 'r')
+    x = x.read()
+    x = x.split('\n')
+    for i in x:
+        sum += int(i.strip().split(',')[colno])
+    return sum/len(x)
+
+def min_col(colno,tno):
+    global tables
+    min_value = 10000000000
+    x = open(tables[tno] + '.csv', 'r')
+    x = x.read()
+    x = x.split('\n')
+    for i in x:
+        num = int(i.strip().split(',')[colno])
+        if num < min_value:
+            min_value = num
+    return min_value
+
+def max_col(colno,tno):
+    global tables
+    max_value = -10000000000
+    x = open(tables[tno] + '.csv', 'r')
+    x = x.read()
+    x = x.split('\n')
+    for i in x:
+        num = int(i.strip().split(',')[colno])
+        if num > max_value:
+            max_value = num
+    return max_value
+
+
+def col_single_op(cno,tno,op):
+    global operations
+    global schema
+    options = {0:sum_col,
+               1:average_col,
+               2:min_col,
+               3:max_col}
+    value = options[op](cno,tno)
+    print operations[op] + '(' + schema[tno][cno+1] + ')'
+    print value
+    return
 
 def kill_engine():
     ct = open("content/bye.txt","r")
@@ -39,8 +95,9 @@ def kill_engine():
     return
 
 
-def print_data(cols,tno):
-    #print cols
+def print_data(cols,ops,tno):
+    print cols
+    print ops
     global tables
     global schema
     data = []
@@ -72,21 +129,46 @@ def process(a):
             tno = find_table(a.tables[0])
             if not tno == -1:
                 cols = []
+                ops = []
+                col_op_query = 0
+                col_not_found = 0
                 error_found = 0
                 if len(a.columns) == 1 and a.columns[0] == "*":
                     cols = range(len(schema[tno])-1)
+                    ops = [-1]*len(cols)
+                elif len(a.columns[0].split('(')) == 2:
+                    col_name = a.columns[0].split('(')[1][:-1]
+                    if find_column(tno,col_name) == -1:
+                        print "One or more columns cannot be found in the table"
+                        col_not_found = 1
+                        error_found = 1
+                    else:
+                        col_op_query = 1
+                        col_single_op(find_column(tno,col_name),tno,operations.index(a.columns[0].split('(')[0]))
+
+
                 else:
                     print "table no " + str(tno)
                     for col in a.columns:
-                        print a.columnssq
-                        if find_column(tno,col) == -1:
-                            print "One or more columns cannot be found in the table"
+                        op = 0
+                        col_name = col
+                        print a.columns
+                        print col_name
+                        if len(col.split('(')) == 2:
+                            print "There is an error in the query.Please recheck"
                             error_found = 1
+                        if find_column(tno,col_name) == -1:
+                            print "One or more columns cannot be found in the table"
+                            col_not_found = 1
                             break
                         else:
-                            cols.append(find_column(tno,col))
-                if not error_found:
-                    print_data(cols,tno)
+                            cols.append(find_column(tno,col_name))
+
+                            if not op:
+                                ops.append(-1)
+
+                if not col_not_found and not error_found and not col_op_query:
+                    print_data(cols,ops,tno)
                 return
             else:
                 print "The table " + a.tables[0] + "  is not found."
