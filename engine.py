6a52d4224d543e6dfcadc4de911ee8a0d37c6bdd
sairam kolla <sai.kolla24@gmail.com>
2016-09-14 04:50:53 +0530

    JOIN operation implemented,Where conditions done

--- engine.py
+++ engine.py
@@ -1,5 +1,6 @@
 from parser import MyParse
 import os
+import copy
 
 schema = []
 tables = []
@@ -104,102 +105,80 @@ def kill_engine():
     print ct.read(),
     return
 
+def rename_where(where_condition,a):
+    if len(where_condition) > 1:
+        cns = where_condition[1:]
 
-def print_data(cols, tno):
-    print cols
-    global tables
-    global schema
-    data = []
-    temp = open(tables[tno] + '.csv', 'r')
-    temp = temp.read()
-    temp = temp.split('\n')
-    table = []
-    for x in temp:
-        table.append(x.strip().split(','))
-    for m in cols[:-1]:
-        print tables[tno] + '.' + str(schema[tno][m + 1]) + ',',
-    print tables[tno] + '.' + str(schema[tno][cols[-1] + 1])
+        #print cns
+        for i in range(0,len(cns)+1,2):
+            for j in [0,2]:
+                temp = cns[i][j]
+                if len(temp.split('.')) == 2:
+                    pass
+                else:
+                    col = cns[i][j]
+                    for table in a.tables:
+                        if not find_column(find_table(table), col) == -1:
+                            cns[i][j] = table + '.' + col
+                            break
+        #print cns
+        return cns
+    return [-1]
 
-    for row in table:
-        temp = []
-        for l in cols:
-            temp.append(row[l])
-        data.append(temp)
-    for row in data:
-        for col in row[:-1]:
-            print col + ',',
-        print row[-1]
+def calculator(a,b,symbol):
+    a = int(a)
+    b = int(b)
+    if symbol == '<':
+        return a < b
+    elif symbol == '>':
+        return a > b
+    elif symbol == '=':
+        return a == b
+
+def evaluate(row,ref_list,where_cn):
+    if len(where_cn) > 1:
+        c1 = where_cn[0]
+        c2 = where_cn[2]
+        op = where_cn[1]
+        if op == 'or':
+            return calculator(row[ref_list.index(c1[0])],row[ref_list.index(c1[2])],c1[1]) or calculator(row[ref_list.index(c2[0])],row[ref_list.index(c2[2])],c2[1])
+        elif op == 'and':
+            return calculator(row[ref_list.index(c1[0])], row[ref_list.index(c1[2])], c1[1]) and calculator(row[ref_list.index(c2[0])], row[ref_list.index(c2[2])], c2[1])
+    else:
+        c1 = where_cn[0]
+        return calculator(row[ref_list.index(c1[0])],row[ref_list.index(c1[2])],c1[1])
+    return 1
+
+def print_data2(query,m,cols_list,ref_list,where_cn):
+    #print "in print data function"
+    if len(query.where[0]) < 1:
+        #print "Where condition is not there"
+        for row in cols_list[:-1]:
+            print row + ',',
+        print cols_list[-1]
+        for row in m:
+            for col in cols_list[:-1]:
+                print row[ref_list.index(col)] + ',',
+            print row[ref_list.index(cols_list[-1])]
+    else:
+
+        print "where condition is there"
+        for row in cols_list[:-1]:
+            print row + ',',
+        print cols_list[-1]
+        for row in m:
+            #print evaluate(row, ref_list, where_cn)
+            if evaluate(row, ref_list, where_cn):
+                for col in cols_list[:-1]:
+                        print row[ref_list.index(col)] + ',',
+                print row[ref_list.index(cols_list[-1])]
+
+            #print where_cn
+
     return
-
 def distinct_data(cols,tno):
     return
-'''
-def process(a):
-    global tables
-    if 1:
-        if len(a.tables) == 1:
-            tno = find_table(a.tables[0])
-            if not tno == -1:
-                cols = []
-                col_op_query = 0
-                col_not_found = 0
-                error_found = 0
-                if len(a.columns) == 1 and a.columns[0] == "*":
-                    cols = range(len(schema[tno]) - 1)
-                    ops = [-1] * len(cols)
-                elif len(a.columns[0].split('(')) == 2:
-                    col_name = a.columns[0].split('(')[1][:-1]
-                    if find_column(tno, col_name) == -1:
-                        print "One or more columns cannot be found in the table"
-                        col_not_found = 1
-                        error_found = 1
-                    else:
-                        col_op_query = 1
-                        col_single_op(find_column(tno, col_name), tno, operations.index(a.columns[0].split('(')[0]))
-                    '''
-                    else:
-                        if a.columns[0].split('(')[0]== "distinct":
-                            cols = []
-                            for x in a.columns:
-                                col_name = a.columns[0].split('(')[1][:-1]
-                                op_name = a.columns[0].split('(')[0]
-                                if find_column(tno,col_name) == -1:
-                                    print "one or more columns not found."
-                                    col_not_found = -1
-                                    error_found = -1
-                                else:
-                        else:
 
-                            col_op_query = 1
-                            col_single_op(find_column(tno, col_name), tno, operations.index(a.columns[0].split('(')[0]))
-                    '''
-                else:
-                    print "table no " + str(tno)
-                    for col in a.columns:
-                        col_name = col
-                        print a.columns
-                        print col_name
-                        if len(col.split('(')) == 2:
-                            print "There is an error in the query.Please recheck"
-                            error_found = 1
-                        if find_column(tno, col_name) == -1:
-                            print "One or more columns cannot be found in the table"
-                            col_not_found = 1
-                            break
-                        else:
-                            cols.append(find_column(tno, col_name))
-
-                if not col_not_found and not error_found and not col_op_query:
-                    print_data(cols, tno)
-                return
-            else:
-                print "The table " + a.tables[0] + "  is not found."
-        else:
-            print "Input a table name."
-    else:
-        print "where condition is not there"
-    return
-'''
 
 def check_tables(tlist):
     global tables
@@ -207,23 +186,133 @@ def check_tables(tlist):
         if table not in tables:
             print "Query cannot be processed. One or more tables is missing."
             return -1
+    if len(set(tlist)) < len(tlist):
+        print "Query cannot be processed. One or more tables are repeating."
+        return -1
     return 1
 
 
 def check_columns(a):
+    if len(a.columns) == 1 and a.columns[0] == "*" and len(a.where[0]) < 1:
+        return 1
+    if not a.columns[0] == "*":
+        for col in a.columns:
+            if len(col.split('.')) ==2:
+                #print col.split('.')
+                col_temp = col.split('.')[1]
+                table = col.split('.')[0]
+                if table in list(a.tables):
+                    if not find_column(find_table(table), col_temp) == -1:
+                        pass
+                    else:
+                        print "Query cannot be processed. One or more columns is not found."
+                else:
+                    print "Query cannot be processed. One or more tables is not found."
+                    return -1
+                continue
+            found = 0
+            for table in a.tables:
+                if  not find_column(find_table(table),col) == -1:
+                    found = 1
+                    break
+            if found == 0:
+                #print col
+                print "Query cannot be processed. One or more columns is not found."
+                return -1
+    #print "for processing where condition"
+    if len(a.where[0]) > 1:
+        #print "checking"
+        cns = copy.deepcopy(a.where[0][1:])
+        for i in range(0,len(cns)+1,2):
+            #print "checking ",i," condition"
+            for j in [0,2]:
+                temp = cns[i][j]
+                if len(temp.split('.')) == 2:
+                    col = temp.split('.')[1]
+                    table = temp.split('.')[0]
+                    #print table
+                    #print a.tables[0]
+                    if table in list(a.tables):
+                            if  not find_column(find_table(table), col) == -1:
+                                pass
+                            else:
+                                print "Query cannot be processed. One or more columns is not found."
+                    else:
+                        print "Query cannot be processed. One or more tables is not found."
+                        return -1
+                else:
+                    col = cns[i][j]
+                    found = 0
+                    for table in a.tables:
+                        if not find_column(find_table(table), col) == -1:
+                            found = 1
+                            break
+                    if found == 0:
+                        #print col
+                        print "Query cannot be processed. One or more columns is not found."
+                        return -1
+
+    return 1
+
+def get_data(a):
+    global schema
+    global tables
+    m = []
+    cols_list = []
+    temp = open(a.tables[0] + '.csv','r')
+    temp = temp.read()
+    temp = temp.split('\n')
+    for x in temp:
+        m.append(x.strip().split(','))
+    cols_list += [a.tables[0] + '.' + x for x in schema[find_table(a.tables[0])][1:]]
+    #print m
+    for table in a.tables[1:]:
+        prev_data = copy.deepcopy(m)
+        m = []
+        temp = open(table + '.csv', 'r')
+        temp = temp.read()
+        temp = temp.split('\n')
+        #print temp
+        for record in prev_data:
+            for x in temp:
+                m.append(record + x.strip().split(','))
+        cols_list +=  [table + '.' + x for x in  schema[find_table(table)][1:]]
+    #print m
+    #print cols_list
+    #print len(m)
+    return m,cols_list
+def rename_columns(a):
+    global schema
+    global tables
+    cols = []
     for col in a.columns:
-        found = 0
-        for table in a.tables:
-            if  find_column(find_table(table),col) == -1:
-                found = 1
-                break
-        if found == 0
-            print "Query cannot be processed. One or more columns is not found."
-            return -1
+        if len(col.split('.')) == 2:
+            cols.append(col)
+        else:
+            for table in a.tables:
+                if not find_column(find_table(table),col) == -1:
+                    cols.append(table + '.' + col)
+                    break
+    return cols
+
 def process(a):
     if not check_tables(a.tables) == -1 and not check_columns(a) == -1:
-        m = get_data(a)
-        print_data(m)
+        m,ref_list = get_data(a)
+        if len(a.columns) == 1 and a.columns[0] == "*":
+            cols_list = ref_list
+        else:
+            cols_list = rename_columns(a)
+        temp  = copy.deepcopy(a.where)[0]
+
+        where_cn = rename_where(temp,a)
+        print a
+        #print m
+        print where_cn
+        print ref_list
+        print cols_list
+        #print len(m)
+
+        print_data2(a,m,cols_list,ref_list,where_cn)
     return
 def run_engine():
     prompt = ">>>  "
@@ -236,7 +325,7 @@ def run_engine():
         elif len(x):
             a = MyParse(x)
             if not a == 0:
-                print a
+                #print a
                 process(a)
         else:
             pass
